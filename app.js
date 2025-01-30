ccac1e67c904318580eeba778b68c88c864dd75e
raymonsilva <raymong888@gmail.com>
2025-01-30 16:28:33 -0300

    Criando uma função que agrega amigos ao Array

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    let amigoInput = document.getElementById('amigo');
+    if (!amigoInput) {
+        alert('Elemento com id "amigo" não encontrado');
+        return;
+    }
+    let amigo = amigoInput.value.trim();
+
+    if (amigo === '') {
+        alert('Por favor, insira um nome');
+        return;
+    }
+
+    amigos.push(amigo);
+    amigoInput.value = '';
+    exibirAmigos();
+}
+
